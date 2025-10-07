7f3072c5ef5693542a325434c000c12e0b9ac156
ed-kung <edward.kung@gmail.com>
2025-10-06 23:43:44 -0700

    report 2025-10-06

--- src/python/globals.py
+++ src/python/globals.py
@@ -8,6 +8,10 @@ sox_id = 26458
 # my user_id
 self_id = 5597
 
+# user_id of someone who spammed freebies
+spammer_id = 30375
+spam_date = "2025-08-25"  # date in which spamming occurred
+
 # important dates and posts
 nc_item_id = 835465 # item id where k00b announced non-custodial 
 nc_date = pd.to_datetime("2025-01-03 19:06:36.050") # date SN went non-custodial
