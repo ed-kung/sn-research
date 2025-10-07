ffffd1f5599d50dfc0ca336198c9453a0e1cd204
ed-kung <edward.kung@gmail.com>
2025-10-07 15:29:40 -0700

    wip

--- src/python/globals.py
+++ src/python/globals.py
@@ -13,6 +13,20 @@ spammer_id = 30375
 spam_date = "2025-08-25"  # date in which spamming occurred
 
 # important dates and posts
-nc_item_id = 835465 # item id where k00b announced non-custodial 
-nc_date = pd.to_datetime("2025-01-03 19:06:36.050") # date SN went non-custodial
+
+# 10% sybil fees
+sf10_item_id = 98002
+sf10_date = pd.to_datetime("2022-11-23 19:12:15.571", utc=True)
+
+# post fees increase to 10 sats
+fee10_item_id = 313522
+fee10_date = pd.to_datetime("2023-11-12 18:39:34.742", utc=True)
+
+# sybil fee up to 30%
+sf30_item_id = 692150 # item id where k00b announced sybil fee going to 30%
+sf30_date = pd.to_datetime("2024-09-19 21:38:43.918", utc=True)
+
+# SN goes non-custodial
+nc_item_id = 835465
+nc_date = pd.to_datetime("2025-01-03 19:06:36.050", utc=True)
 
