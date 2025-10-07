40d119693346ced0fd5cbed26afe54fd54950a61
ed-kung <edward.kung@gmail.com>
2025-10-06 18:38:04 -0700

    wip 2025-10-06

---
+++ src/python/globals.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# user_ids for SN founder and employees
+k00b_id = 616
+ek_id = 6030
+sox_id = 26458
+
+# my user_id
+self_id = 5597
+
+# important dates and posts
+nc_item_id = 835465 # item id where k00b announced non-custodial 
+nc_date = pd.to_datetime("2025-01-03 19:06:36.050") # date SN went non-custodial
+
