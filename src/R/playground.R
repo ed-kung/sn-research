391abacec0de94eec7b9b3ddd179e0693bf22c15
ed-kung <edward.kung@gmail.com>
2025-10-11 10:14:21 -0700

    wip

--- src/R/playground.R
+++ src/R/playground.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 library(here)
 library(lfe)
 library(yaml)
@@ -6,17 +8,20 @@ library(stargazer)
 LOCAL_CONFIG <- read_yaml("config.yaml.local")
 DATA_PATH <- LOCAL_CONFIG["DATA_PATH"][[1]]
 
-filename <- paste0(DATA_PATH, "/territory_by_week_panel.csv")
+filename <- paste0(DATA_PATH, "/posts_quality_regs.csv")
 
 df <- read.csv(filename)
 
-df$log_posts <- log(1+df$n_posts)
-df$log_fee <- log(df$posting_fee)
+df$log_sats48 <- log(1+df$sats48)
+df$log_cost <- log(df$cost)
 
-reg <- felm(log_posts ~ log_fee | subId + weekId, data=df)
+reg0 <- felm(log_sats48 ~ log_cost, data=df)
+reg1 <- felm(log_sats48 ~ log_cost | subId, data=df)
+reg2 <- felm(log_sats48 ~ log_cost | subId + weekId, data=df)
+reg3 <- felm(log_sats48 ~ log_cost | subId + weekId + userId, data=df)
 
-stargazer(reg, type="text")
+stargazer(reg0, reg1, reg2, reg3, type="text")
 
-fe_df <- getfe(reg)
-fe_filename <- paste0(DATA_PATH, "/territory_by_week_fe.csv")
+fe_df <- getfe(reg3)
+fe_filename <- paste0(DATA_PATH, "/posts_quality_regs_fe.csv")
 write.csv(fe_df, fe_filename, row.names=FALSE)
